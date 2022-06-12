05f296cb93d5f42bb43a5d1e9f38e9e0f2d10e95
Benjamin Akar <benjaminakar2001@gmail.com>
2022-06-12 15:52:07 +0200

    fix: draggable window

--- packages/interface/src/screens/Content.tsx
+++ packages/interface/src/screens/Content.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 
 export const ContentScreen: React.FC<{}> = (props) => {
 	return (
-		<div className="flex flex-col w-full h-screen p-5 custom-scroll page-scroll">
-			<div className="flex flex-col space-y-5 pb-7">
+		<div className="flex flex-col w-full h-screen custom-scroll page-scroll">
+			<div data-tauri-drag-region className="flex flex-shrink-0 w-full h-5" />
+			<div className="flex flex-col p-5 space-y-5 pb-7">
 				<p className="px-5 py-3 mb-3 text-sm text-gray-400 rounded-md bg-gray-50 dark:text-gray-400 dark:bg-gray-600">
 					<b>Note: </b>This is a pre-alpha build of Spacedrive, many features are yet to be
 					functional.
