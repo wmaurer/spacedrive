270ca9ae56d1a3eb5124d2d12b55e2643df35488
Benjamin Akar <benjaminakar2001@gmail.com>
2022-06-14 18:24:18 +0200

    refactor: slight improvement to albums

--- packages/interface/src/screens/Photos.tsx
+++ packages/interface/src/screens/Photos.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import { ReactComponent as Image } from '../assets/svg/image.svg';
+import { TopBar } from '../components/layout/TopBar';
 
 interface AlbumProps {
 	id: number;
@@ -14,24 +15,26 @@ function Album(props: AlbumProps) {
 
 	return (
 		<article className="group cursor-pointer">
-			<div className="relative w-52 h-28 rounded-md overflow-hidden border-2 border-[#1E1E27]">
-				<div className="absolute z-10 left-0 top-0 flex items-center space-x-1 bg-[#1E1E27] w-fit px-2 rounded-br-md">
-					<Image />
-					<span className="text-sm">{images.length}</span>
-				</div>
-				<div className="flex">
-					{images.map((src) => (
-						<div
-							className="w-1/3 h-40 group-hover:scale-110 duration-300 no-repeat bg-center bg-cover"
-							style={{
-								backgroundImage: `url(${src})`
-							}}
-							key={src}
-						></div>
-					))}
-				</div>
+			<div className="relative w-36 h-32 rounded-md">
+				<div
+					className="w-full rounded-md h-32 duration-300 no-repeat bg-center bg-cover"
+					style={{
+						backgroundImage: `url(${images[0]})`
+					}}
+					key={images[0]}
+				></div>
 			</div>
-			<h3 className="text-sm mt-1 group-hover:opacity-60 duration-300">{title}</h3>
+			<h3
+				style={{
+					fontSize: 14
+				}}
+				className="font-semibold mt-1 group-hover:opacity-60 duration-300"
+			>
+				{title}
+			</h3>
+			<h4 className="text-sm mt-0 text-gray-300 group-hover:opacity-60 duration-300">
+				{images.length}
+			</h4>
 		</article>
 	);
 }
@@ -41,32 +44,24 @@ export const PhotosScreen: React.FC<{}> = (props) => {
 	const albums = [
 		{
 			id: 1,
-			title: 'Album 1',
-			images: [
-				'https://picsum.photos/id/1/200/300',
-				'https://picsum.photos/id/2/200/300',
-				'https://picsum.photos/id/3/200/300'
-			]
+			title: 'Selfies',
+			images: ['https://pbs.twimg.com/profile_images/1516440650471944192/sbA26BqA_400x400.jpg']
 		},
 		{
 			id: 2,
 			title: 'Album 2',
-			images: [
-				'https://picsum.photos/id/5/200/300',
-				'https://picsum.photos/id/6/200/300',
-				'https://picsum.photos/id/7/200/300'
-			]
+			images: ['https://picsum.photos/id/10/200/300']
 		}
 	];
 
 	return (
 		<div className="flex flex-col w-full h-screen overflow-x-hidden custom-scroll page-scroll">
-			{/* <TopBar /> */}
+			<TopBar />
 			<div data-tauri-drag-region className="flex flex-shrink-0 w-full h-5" />
-			<div className="flex flex-col p-5 space-y-5 pb-7">
+			<div className="flex flex-col p-5 pt-0 space-y-5 pb-7">
 				<h1 className="text-xl font-bold">Photos</h1>
 				<h2 className="text-lg font-bold">Albums</h2>
-				<div className="flex space-x-2">
+				<div className="flex space-x-4">
 					{albums.map((album) => (
 						<Album {...album} key={album.id} />
 					))}
